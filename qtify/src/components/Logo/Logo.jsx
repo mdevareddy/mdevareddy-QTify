44825f560b962a6a2b83cf27f70ff200cc7a0a35
mdevareddy <mdevareddy77@gmail.com>
2024-06-25 12:02:00 +0530

    test commit

--- qtify/src/components/Logo/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Qtify from "../../assets/Q tify.png";
-
-const Logo = () => {
-  return <img src={Qtify} alt='Logo' width='67px' height='34px'></img>;
-};
-
-export default Logo;
